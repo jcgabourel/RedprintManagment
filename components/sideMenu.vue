bbffa307ceda7f0a8bdb50b725df62ff4bcb3381
jcgabourel <40446669+jcgabourel@users.noreply.github.com>
2023-11-01 13:06:56 -0500

    agrega proveedores al menu

--- components/sideMenu.vue
+++ components/sideMenu.vue
@@ -15,18 +15,36 @@
           <template v-slot:activator="{ props }">
             <v-list-item v-bind="props" title="Stock"></v-list-item>
           </template>
-          <v-list-item value="existencias" title="Existencias" to="/existencias" />
-          <v-list-item value="movimientos" title="Movimientos" to="/movimientos" />
+          <v-list-item
+            value="existencias"
+            title="Existencias"
+            to="/existencias"
+          />
+          <v-list-item
+            value="movimientos"
+            title="Movimientos"
+            to="/movimientos"
+          />
         </v-list-group>
+      </v-list-group>
+      <v-list-group value="Compras">
+        <template v-slot:activator="{ props }">
+          <v-list-item
+            v-bind="props"
+            prepend-icon="mdi-account-circle"
+            title="Compras"
+          ></v-list-item>
+        </template>
+        <v-list-item value="Compras" title="Compras" to="/compras" />
+      </v-list-group>
 
-        <v-list-group value="catalogos">
-          <template v-slot:activator="{ props }">
-            <v-list-item v-bind="props" title="Catalogos"></v-list-item>
-          </template>
-          <v-list-item value="categorias" title="Caegorias" to="/categorias" />
-          <v-list-item value="marcas" title="Marcas" to="/marcas" />
-          <v-list-item value="locaciones" title="Locaciones" to="/locaciones" />
-        </v-list-group>
+      <v-list-group value="catalogos">
+        <template v-slot:activator="{ props }">
+          <v-list-item v-bind="props" title="Catalogos"></v-list-item>
+        </template>
+        <v-list-item value="categorias" title="Caegorias" to="/categorias" />
+        <v-list-item value="marcas" title="Marcas" to="/marcas" />
+        <v-list-item value="locaciones" title="Locaciones" to="/locaciones" />
       </v-list-group>
     </v-list>
   </div>
